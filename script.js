e30dadaaff0fb3439153b90130f8c0a01d669be1
username <ivlevaarina@gmail.com>
2024-11-21 09:00:29 +0600

    like

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById('like').addEventListener('click', function () {
-	if ((document.getElementById('like').src = 'img/like.svg')) {
+	if (document.getElementById('like').getAttribute('src') == 'img/like.svg') {
 		document.getElementById('like').src = 'img/redLike.svg'
 	} else {
 		document.getElementById('like').src = 'img/like.svg'
 	}
 })
+
+console.log(document.getElementById('like').getAttribute('src'))
