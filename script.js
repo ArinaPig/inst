e0393b34f8f38876df8c2c476ff9936189355725
username <ivlevaarina@gmail.com>
2024-11-21 09:10:26 +0600

    like

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+localStorage.setItem('redLike', 'img/redLike.svg')
+localStorage.setItem('like', 'img/like.svg')
+
 document.getElementById('like').addEventListener('click', function () {
 	if (document.getElementById('like').getAttribute('src') == 'img/like.svg') {
-		document.getElementById('like').src = 'img/redLike.svg'
+		document.getElementById('like').src = localStorage.getItem('redLike')
 	} else {
-		document.getElementById('like').src = 'img/like.svg'
+		document.getElementById('like').src = localStorage.getItem('like')
 	}
 })
